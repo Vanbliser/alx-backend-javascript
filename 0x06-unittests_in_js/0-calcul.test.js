758a0d86de22d02823155deb7099299674efd81a
Blossom Israel Ayogu <ayogublossomisrael@gmail.com>
2024-11-07 03:59:04 +0100

    Task 0

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -5,7 +5,10 @@ describe('calculateNumber', () => {
   it('should return the rounded sum of two numbers', () => {
     assert.strictEqual(calculateNumber(1, 3), 4);
     assert.strictEqual(calculateNumber(1, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(1, 3.2), 4);
+    assert.strictEqual(calculateNumber(1.2, 3), 4);
+    assert.strictEqual(calculateNumber(1.7, 3), 5);
+    assert.strictEqual(calculateNumber(1.2, 3.2), 4);
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
 });
